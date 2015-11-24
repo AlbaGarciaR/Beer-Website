8deb5844ab361d5d9de9a6b7aeed38b8171ae10e
AlbaGarciaR <algardriguez@gmail.com>
2015-11-24 17:08:39 +0100

    Creation about_us.php

---
+++ about_us.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<!-- En-tête de la page -->
+		<link rel="stylesheet" href="style.css" />
+		<meta charset="utf-8" />
+		<title>Bières</title>
+	</head>
+
+	<body>
+				
+<header>
+<?php include("menu.php"); ?>
+</header>
+
+	<section id=fond>
+		
+				<h1>A propos de nous</h1>
+                <p>Nous sommes 4 étudiants de l’Ecole Centrale de Lyon, tous anciens membres d’une association de Zythologie dans le nord de la France, et passionnés par cette extraordinaire boisson qu'est la bière.
+                    Nous avons été amenés lors d’un projet informatique à créer un site répertoriant un grand nombre de bières. Nous avons été ravis de le confectionner et espérons qu’il vous apportera l’aide que vous êtes certainement venu chercher.
+                </p></br>
+			<center><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5564.76703101825!2d4.7688084!3d45.78354385!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4ec9529579211%3A0xf9dafe9c0d9620e3!2s%C3%89cole+Centrale+de+Lyon!5e0!3m2!1sfr!2sfr!4v1433576186643" width="800" height="600" frameborder="0" style="border:0"></iframe></center>
+				
+	</section>
+
+	</body>
+	
+	<footer>
+				</br></br></br></br>Site Web en developpement - Ecole Centrale de Lyon - 36 Avenue Guy de Collongue 69134 Ecully</br></br></br></br>
+	</footer>
+	
+</html>
+	
