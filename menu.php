511a0b8ad43aaf96bd30fb858edc9093b6784afe
AlbaGarciaR <algardriguez@gmail.com>
2015-11-24 17:19:41 +0100

    Creation menu.php

---
+++ menu.php
@@ -0,0 +1,13 @@
+<div id="banniere_image">
+		<nav id="nav">
+			<ul id=nav_ul>
+				<li id=nav_li><a href="site.php">Accueil</a></li>
+				<li id=nav_li><a href="bieres.php">Nos Bières</a></li>
+				<li id=nav_li><a href="choisir.php">Choisir</a></li>
+				<li id=nav_li><a href="selection.php">Sélection</a></li>
+				<li id=nav_li><a href="connexion.php">Connexion</a></li>
+				<li id=nav_li><a href="about_us.php">About us</a></li>
+
+			</ul>
+	</nav>
+</div>
