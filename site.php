f85e32caae6bdb0d3053908ceec4d8fcdb1b1ddb
AlbaGarciaR <algardriguez@gmail.com>
2015-11-24 17:22:11 +0100

    Creation site.php

---
+++ site.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<!-- En-tête de la page -->
+		<link rel="stylesheet" href="style.css" />
+		<meta charset="utf-8" />
+		<title>Bières</title>
+	</head>
+
+	<body>
+				
+	<header>
+			<?php include("menu.php"); ?>
+	</header>
+
+    <section>
+        <!--baniere-->
+	</section>
+
+<span id="sl_play" class="sl_command"></span>
+<span id="sl_pause" class="sl_command"></span>
+
+<section id="slideshow">
+
+<a class="play_commands pause" href="#sl_pause" title="Maintain paused">Pause</a>
+<a class="play_commands play" href="#sl_play" title="Play the animation">Play</a>
+
+    <div class="container">
+        <div class="c_slider"></div>
+            <div class="slider">
+                <figure>
+                    <img src="Images/accueil1.png" alt="" width="1020" height="768" />
+                    <figcaption>Bienvenue sur notre site! </figcaption>
+
+                </figure>
+                <figure>
+                    <img src="Images/accueil2.jpg" alt="" width="1020" height="768" />
+                    <figcaption>Ici vous pouvez découvrir les meilleures bières du monde
+                    </figcaption>
+                </figure>
+                <figure>
+                    <img src="Images/brasserie.jpg" alt="" width="1020" height="768" />
+                    <figcaption>blablabla</figcaption>
+
+                </figure>
+                <figure>
+                    <img src="Images/delirium.jpg" alt="" width="1020" height="768" />
+                    <figcaption>Délirium Bar à Bruxelles,élu Meilleur Bar du Monde</figcaption>
+                </figure>
+            </div>
+    </div>
+
+    <span id="timeline"></span>
+
+</section>
+
+<ul id=fond>
+	<section>
+
+				<h2>A propos du site</h2>
+<p>Bonjour à tous.</br> Nous vous souhaitons la bienvenue sur notre site !
+La bière… Que dire sur cette fantastique invention ? Plus que la simple boisson alcoolisée obtenue par la fermentation de matières glucidiques, elle est au centre d’une science infiniment passionnante et injustement méconnue : la zythologie. Blonde ou brune, forte ou légère, belge ou française… Jamais vous ne trouverez deux bières semblables, malgré leur extraordinaire diversité.
+Sur ce site, découvrez la bière qui vous correspond ! Choisissez les critères de la bière désirée, et nous vous la proposerons.
+Nous vous souhaitons une bonne visite. A votre santé !
+</p>
+				
+	</section>
+</ul>
+	</body>
+	
+	<footer>
+				</br></br></br></br>Site Web en developpement - Ecole Centrale de Lyon - 36 Avenue Guy de Collongue 69134 Ecully</br></br></br></br>
+	</footer>
+	
+</html>
+	
